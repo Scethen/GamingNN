42d9144089fdc23e4b5c179a9fc01862e64e8eb1
SpicyNova <abby.ortego@selu.edu>
2023-05-06 01:39:48 -0500

    WIP: connection of NN to Game

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ import random
 import numpy as np
 import pandas as pd
 from IPython.display import display
+import layer 
 
 # %% INITIAL VARS
 # Switch between simulation and game mode
@@ -376,8 +377,8 @@ def closestBomb(listofbombs,turrentx,turrenty):
                     angle = angle +180
     return distance/700,angle/180
 
-# %% LEARN GAME
-def learnGame(controls,numberofcontrolls):
+# %% LEARN GAME FR
+def learnGame_ForRealsies(controls):
     
     
     # Count the number of game loops.
@@ -481,8 +482,316 @@ def learnGame(controls,numberofcontrolls):
             ang = t.getGunAngle()-180
             closestEnemyDistance, closestEnemyAngle = closestEnemy(turrentx=t.x,turrenty=t.y,listoftargets=targets)
             closestBombDistance, closestBombAngle = closestBomb(turrentx=t.x,turrenty=t.y,listofbombs=bombs)
-            print(ang,closestEnemyAngle,closestEnemyDistance,closestBombDistance,closestBombAngle)
+            # print(ang/180,closestEnemyAngle,closestEnemyDistance,closestBombDistance,closestBombAngle)
 
+            
+            output = controls.calcWeightedInput([ang,closestEnemyAngle,closestEnemyDistance,closestBombDistance,closestBombAngle])
+            # print(output)
+            final = controls.activation(output)
+            # print(final)
+            if (final[0] == 1): 
+                t.moveMe(1)
+            if (final[1] == 1): 
+                t.moveMe(-1)
+            if (final[2] == 1): 
+                t.rotateMe(-1)
+            if (final[3] == 1): 
+                t.rotateMe(1)
+            if (final[4] == 1):
+                gx, gy = t.getGunTip()
+                ang = t.getGunAngle()
+                bullets.append(bullet(gx, gy, ang))
+                bulletsShot = bulletsShot + 1
+            #
+        #
+                
+        # If in simulation mode, turrets actoins are controlled by
+        # other means.
+        else:
+            ang = t.getGunAngle()-180
+            closestEnemyDistance, closestEnemyAngle = closestEnemy(turrentx=t.x,turrenty=t.y,listoftargets=targets)
+            closestBombDistance, closestBombAngle = closestBomb(turrentx=t.x,turrenty=t.y,listofbombs=bombs)
+            #print(ang,closestEnemyAngle,closestEnemyDistance,closestBombDistance,closestBombAngle)
+
+            
+            output = controls.calcWeightedInput([ang,closestEnemyAngle,closestEnemyDistance,closestBombDistance,closestBombAngle])
+            # print(output)
+            final = controls.activation(output)
+            #print(final)
+            if (final[0] == 1): 
+                t.moveMe(1)
+            if (final[1] == 1): 
+                t.moveMe(-1)
+            if (final[2] == 1): 
+                t.rotateMe(-1)
+            if (final[3] == 1): 
+                t.rotateMe(1)
+            if (final[4] == 1):
+                gx, gy = t.getGunTip()
+                ang = t.getGunAngle()
+                bullets.append(bullet(gx, gy, ang))
+                bulletsShot = bulletsShot + 1
+            #
+        #
+            
+        # --- Game logic should go here
+        
+        # See if a target drops a bomb and see what it selects for a move.
+        numTargs = len(targets)
+        if (numTargs > 1):
+            j = 0
+            for targ in targets:
+                if (targ.doIExist() == True):
+                    # Drop bomb.
+                    myChance = random.randint(0, 100)
+                    if (myChance < launchBomb):
+                        bombBayX, bombBayY = targ.getBombBay()
+                        bombs.append(bomb(bombBayX, bombBayY))
+                    
+                    # Target move.
+                    # Find closest target to this target so we do not
+                    # run into it.
+                    if (j == 0):
+                        closeTargX = targets[j+1].x
+                    elif (j == (numTargs - 1)):
+                        closeTargX = targets[j-1].x
+                    else:
+                        lDiff = abs(targ.x - targets[j-1].x)
+                        rDiff = abs(targ.x - targets[j+1].x)
+                        
+                        closeTargX = targets[j-1].x
+                        if (rDiff < lDiff):
+                            closeTargX = targets[j+1].x
+                            
+                    # Figure out which way to move.
+                    myMove = targ.what2Do(closeTargX, targWidth, t.x, 1)
+                    # Move.
+                    targ.moveMe(myMove)
+                    
+                    # Update j.
+                    j = j+1
+                    
+        elif (numTargs == 1):
+            # Drop bomb.
+            myChance = random.randint(0, 100)
+            if (myChance < launchBomb):
+                bombBayX, bombBayY = targ.getBombBay()
+                bombs.append(bomb(bombBayX, bombBayY))
+                
+            # Move Target based on turret position.
+            if (t.x < targets[0].x):
+                myMove = left
+            elif (t.x > targets[0].x):
+                myMove = right
+            else:
+                myMove = noMove
+                
+            # Move.
+            targets[0].moveMe(myMove)
+           
+        # --- Move bullets. 
+        for b in bullets:
+            b.moveMe()
+            if (b.doIExist() == False):
+                bullets.remove(b)
+                # print(len(bullets))
+                
+        # --- Move bombs.
+        for B in bombs:
+            B.moveMe()
+            if (B.doIExist() == False):
+                bombs.remove(B)
+        
+        # --- Check to see if the bullets hit anything
+        
+        # Did a bullet hit a bomb?
+        for b in bullets:
+            for B in bombs:
+                if (b.hit == False):
+                    if (B.exists == True):
+                        b.hit = collideBomb(b.x, b.y, b.radius, B.x, B.y, B.radius)
+                        if (b.hit == True):
+                            B.hit = True
+                            bombsShotDown = bombsShotDown + 1
+                 
+        # Did a bullet hit a target?
+        for b in bullets:
+            for targ in targets:
+                if (b.hit == False):
+                    if (targ.exists == True):
+                        b.hit = collideTarget(b.x, b.y, b.radius, targ.x, targ.y, targ.width, targ.height)
+                        if (b.hit == True):
+                            targ.hitCount = targ.hitCount + 1
+                            bulletHits = bulletHits + 1
+                          
+        # Did a bomb hit the turret?
+        for B in bombs:
+            if ((B.exists == True) and (B.hit == False)):
+                B.hit = collideBomb(B.x, B.y, B.radius, t.x, t.y, t.rad)
+                if (B.hit == True):
+                    t.hitCount = t.hitCount + 1
+                    
+        # Remove old targets.
+        for targ in targets:
+            if (targ.exists == False):
+                targets.remove(targ)
+                targetsKilled = targetsKilled + 1      
+                # print("num targets = ", len(targets))
+           
+        # --- Screen-clearing code goes here
+     
+        # Here, we clear the screen to black. Don't put other drawing commands
+        # above this, or they will be erased with this command.
+     
+        # If you want a background image, replace this clear with blit'ing the
+        # background image.
+        if (GAME_MODE) :
+            screen.fill(BLACK)
+
+        # --- Drawing code should go here
+        # Draw turret.
+        t.drawMe(screen)
+        
+        # Draw bullets.
+        for b in bullets:
+            b.drawMe(screen)
+
+        # Draw targets.
+        for targ in targets:
+            targ.drawMe(screen)
+            
+        # Draw bombs.
+        for B in bombs:
+            B.drawMe(screen)
+                
+        # --- Go ahead and update the screen with what we've drawn.
+        if (GAME_MODE):
+            p.display.flip()
+            # Only delay in game mode.
+            clock.tick(60)
+        
+        if ((t.exists == False)or(len(targets) == 0)):
+            running = False
+            # print("Game Over")
+            # Here is where you print the game statistics.
+            # print("loopcount: ", loopcount)
+            # print("turret was hit this many times: ", turrethitCount)
+            # print("bullets shot: ", bulletsShot)
+            # print("bullet hits:", bulletHits)
+            # print("bombs shot down: ", bombsShotDown)
+            # print("targets killed: ", targetsKilled)
+            
+        loopcount = loopcount + 1
+
+    # Close the window and quit.
+    p.quit()
+    
+    return np.array([loopcount, t.hitCount, bulletsShot, bulletHits, bombsShotDown, targetsKilled])
+# LEARN GAME
+
+# %% LEARN GAME TRAIN
+def learnGameTrain(controls, numberofcontrolls):
+    
+    
+    # Count the number of game loops.
+    loopcount = 0
+    
+    # Game stats
+    bulletsShot = 0
+    bulletHits = 0
+    bombsShotDown = 0
+    targetsKilled = 0
+    
+    # Initialize random.
+    random.seed()
+    
+    # Set the width and height of the screen [width, height] .
+    size = (screenWidth, screenHeight)
+    
+    # Set up screen and whatnot.
+    screen = None
+    if (GAME_MODE):
+        p.init()
+        screen = p.display.set_mode(size)
+        p.display.set_caption("learnGame()")
+    
+    # Turret postion.
+    turrX = (int)(screenWidth/2)
+    turrY = screenHeight - 50
+    # Create turret
+    t = turret(turrX, turrY, 20)
+    
+    # Create the targets.
+    targets = []
+    
+    if (maxTargets < 1):
+        print("Gun Training")
+    elif (maxTargets == 1):
+        myX = int(screenWidth/2)
+        myY = 20
+        myTarget = target(myX, myY, 90)
+        targets.append(myTarget)
+    else:
+        for j in range(maxTargets):
+            if (j == 0):
+                myX = 20
+                myY = 20
+                targXInc = int((screenWidth - 40)/(maxTargets-1))
+                
+            else:
+                myX = myX + targXInc
+                myY = 20
+                
+            myTarget = target(myX, myY, 90)
+            targets.append(myTarget)
+    
+    # Create bullet array.
+    bullets = []
+    
+    # Create a bomb array.
+    bombs = []
+    
+    # Loop until the user clicks the close button.
+    running = True 
+     
+    # Used to manage how fast the screen updates
+    clock = p.time.Clock()
+    
+    # -------- Main Program Loop -----------
+    count = 0
+    while running:
+        # --- Main event loop
+        
+        # If playing a game, process keystrokes to control the
+        # turret.
+        if (GAME_MODE):
+            for event in p.event.get():
+                if event.type == p.QUIT:
+                    running = False
+        
+            """ Check for keyboard presses. """
+            # key = p.key.get_pressed()
+            
+            # if (key[p.K_ESCAPE] == True): 
+            #     running = False
+            
+            # Actions that the turret can take.  The
+            # user controls this, or a smart controller
+            # can control these.
+            # if (key[p.K_UP] == True): 
+            #     t.moveMe(1)
+            # if (key[p.K_DOWN] == True): 
+            #     t.moveMe(-1)
+            # if (key[p.K_LEFT] == True): 
+            #     t.rotateMe(-1)
+            # if (key[p.K_RIGHT] == True): 
+            #     t.rotateMe(1)
+            # if (key[p.K_SPACE] == True):
+            #     gx, gy = t.getGunTip()
+            #     ang = t.getGunAngle()
+            #     bullets.append(bullet(gx, gy, ang))
+            #     bulletsShot = bulletsShot + 1
+            
             if (controls[count][0] == 1): 
                 t.moveMe(1)
             if (controls[count][1] == 1): 
@@ -673,7 +982,7 @@ def learnGame(controls,numberofcontrolls):
     p.quit()
     
     return np.array([loopcount, t.hitCount, bulletsShot, bulletHits, bombsShotDown, targetsKilled])
-# LEARN GAME
+# TRAIN
 
 # %% GENETIC ALG
 def score(stats):
@@ -699,64 +1008,105 @@ def breed(parent1, parent2):
     return kid
 #
 
-def geneticAlg(nGen, popSize):
+def calcWeightedInput(inputs, weights):
+    # assumes...
+    # len(weights[i]) = len(inputs)
 
+    # multiply weights[i] * inputs = a single WeightedInput 
+    WeightedInput = [sum(weight * inputs) for weight in weights]
+
+    return WeightedInput
+#
+
+def activation(inputs):
+
+    for indx, input in enumerate(inputs):
+        inputs[indx] = 1 / (1 + m.exp(-input))
+
+        if(input < 1):
+            inputs[indx] = 0
+        else:
+            inputs[indx] = 1
+        #
+    #
+
+    return inputs
+#
+
+def geneticAlg(nGen, popSize):
+    
     # for each generation in nGen...
     for gen in range(nGen):
         # if first generation...
         if gen == 0:
             # ...create first rndm population >> bool arr, len 5, filled with 0 and 1
-            pop = np.array([[np.random.randint(2, size = 5) for x in range(1000)] for y in range(popSize)])
+            # pop = np.array([[np.random.randint(2, size = 5) for x in range(1000)] for y in range(popSize)])
+            # pop = np.array([layer.layer(np.zeros((5), dtype = float), [np.random.random_sample(size = 5) for node in range(0,5)]).weights for individ in range(popSize)])
+            pop = [[np.random.random_sample(size = 5) for node in range(0,5)] for individ in range(popSize)]
         #
 
+        # neural net feed
+        inputs = np.random.random_sample(size = 5)
+        outputs = [calcWeightedInput(inputs, individ) for individ in pop] # weights per perceptron, per individual in pop
+        controls = [activation(output) for output in outputs]
+
         # get stats for each individual in the population
-        stats = np.array([learnGame(individ,1000) for individ in pop])
-        #print(stats)
+        stats = np.array([learnGameTrain(control, popSize) for control in controls])
+        print(stats)
         
-        # score each individual based on game stats
-        pop_score = np.array([score(stat) for stat in stats])
-        #print("\n\n",pop_score)
+    #     # score each individual based on game stats
+    #     pop_score = np.array([score(stat) for stat in stats])
+    #     #print("\n\n",pop_score)
 
-        pop_score_df = pd.DataFrame(pop_score,columns = ['score'])
-        stats_df = pd.DataFrame(data = stats, columns = ['loopcount','hitten','bullest shot','bullet hits',
-                                              'bombs shot','targets killed'])
-        result_df = pop_score_df.join(stats_df).sort_values(by=['score'])
-        # rank population >> sort()
-        sort = np.argsort(pop_score) # indx of sorted pop score
-        # print(stats[sort])
-        pop = pop[sort] # pop ranked by score sort >> min to max
-        #print(pop)
+    #     pop_score_df = pd.DataFrame(pop_score,columns = ['score'])
+    #     stats_df = pd.DataFrame(data = stats, columns = ['loopcount','hitten','bullest shot','bullet hits',
+    #                                           'bombs shot','targets killed'])
+    #     result_df = pop_score_df.join(stats_df).sort_values(by=['score'])
+    #     # rank population >> sort()
+    #     sort = np.argsort(pop_score) # indx of sorted pop score
+    #     # print(stats[sort])
+    #     pop = pop[sort] # pop ranked by score sort >> min to max
+    #     #print(pop)
 
-        # generate new population >>>
-        # keep certain top percentage of population
-        top = np.array(pop[:int(len(pop) * 0.3)]) # highest scores from top 20%
-        # loop through bottom percentage of population...
-        for individidx in range(int(len(pop)*.3),int(len(pop))):
-            # choose two parents randomly from top percent
-            parent1 = top[random.randint(0, len(top)-1)]
-            parent2 = top[random.randint(0, len(top)-1)]
+    #     # generate new population >>>
+    #     # keep certain top percentage of population
+    #     top = np.array(pop[:int(len(pop) * 0.3)]) # highest scores from top 20%
+    #     # loop through bottom percentage of population...
+    #     for individidx in range(int(len(pop)*.3),int(len(pop))):
+    #         # choose two parents randomly from top percent
+    #         parent1 = top[random.randint(0, len(top)-1)]
+    #         parent2 = top[random.randint(0, len(top)-1)]
 
-            # produce a kid >> breed(parent1, parent2)
-            kid = breed(parent1, parent2)
+    #         # produce a kid >> breed(parent1, parent2)
+    #         kid = breed(parent1, parent2)
 
-            # replace current individual with kid
-            pop[individidx] = kid
-        #...fin
-        print('Gen:',gen)
-        result_mean = result_df.mean().to_frame().T
-        display(result_mean)
+    #         # replace current individual with kid
+    #         pop[individidx] = kid
+    #     #...fin
+    #     print('Gen:',gen)
+    #     result_mean = result_df.mean().to_frame().T
+    #     display(result_mean)
 
-    #..fin
-    print(stats_df)
-    display(result_df)
-    return top[0]
+    # #..fin
+    # print(stats_df)
+    # display(result_df)
+    return # top[0]
 #
 
+# %% MAIN
 # loopcount, turrethitCount, bulletsShot, bulletHits, bombsShotDown, targetsKilled = learnGame()
-sample_controls = geneticAlg(10, 100)
-print(sample_controls)
+# sample_controls = geneticAlg(10, 100)
+# print(sample_controls)
+# GAME_MODE = True
+# print(learnGame(sample_controls,1000))
 
-GAME_MODE = True
-print(learnGame(sample_controls,1000))
+# inputs = np.random.random_sample(size = 5)
+# layer1 = layer.layer([np.random.random_sample(size = 5) for node in range(0,5)])
+# output = layer1.calcWeightedInput(inputs)
+# final = layer1.activation(output)
+# GAME_MODE = True
+# print(learnGame(layer1))
 
-# %%
+# breed weights still use learngame as fitness
+
+geneticAlg(10, 100)
--- layer.py
+++ layer.py
@@ -1,11 +1,13 @@
 # %% IMPORTS
 import numpy as np
+import math as m
 
 # %% layer
 class layer():
 
-    def __init__(self, nodes):
+    def __init__(self, nodes, weights):
         self.nodes = nodes # originally weights
+        self.weights = weights
     #
 
     def calcWeightedInput(self, inputs):
@@ -13,7 +15,7 @@ class layer():
         # len(weights[i]) = len(inputs)
 
         # multiply weights[i] * inputs = a single WeightedInput 
-        WeightedInput = [sum(node * inputs) for node in self.nodes]
+        WeightedInput = [sum(weight * inputs) for weight in self.weights]
 
         return WeightedInput
     #
@@ -21,6 +23,8 @@ class layer():
     def activation(self, inputs):
 
         for indx, input in enumerate(inputs):
+            inputs[indx] = 1 / (1 + m.exp(-input))
+
             if(input < 1):
                 inputs[indx] = 0
             else:
@@ -33,15 +37,15 @@ class layer():
 
 # layer
 
-# %%
-inputs = np.random.random_sample(size = 5)
-print(inputs)
+# %% 
+# inputs = np.random.random_sample(size = 5)
 
-layer1 = layer([np.random.random_sample(size = 5) for node in range(0,5)])
-print(layer1.nodes)
+# layer1 = layer(np.zeros((5), dtype = float), [np.random.random_sample(size = 5) for node in range(0,5)])
+# print(layer1.weights)
+# print(layer1.nodes)
 
-output = layer1.calcWeightedInput(inputs)
-print(output)
+# output = layer1.calcWeightedInput(inputs)
+# print(output)
 
-final = layer1.activation(output)
-print(final)
+# final = layer1.activation(output)
+# print(final)
